0069aef7cf9b34e05146adcbf5b54f012aaa4367
Krishnaaccolite <152381768+Krishnaaccolite@users.noreply.github.com>
2023-12-11 09:45:55 +0530

    Update WayneEnterprises.java

--- Question-3_WayneEnterprises/src/WayneEnterprises.java
+++ Question-3_WayneEnterprises/src/WayneEnterprises.java
@@ -9,15 +9,16 @@ public class WayneEnterprises {
         LinkedBlockingQueue<Order> Ordered_queue = new LinkedBlockingQueue<>();
         ExecutorService executorService = Executors.newFixedThreadPool(12); // 7 customers + 5 ships
         // Create customer threads
-        for (int i = 0; i < 7; i++) {
-            executorService.submit(new Customer(Ordered_queue
-));
+        int i=0;
+        while(i<7) {
+            executorService.submit(new Customer(Ordered_queue));
+            i++;
         }
+        i=0;
         // Create ship threads
-        for (int i = 0; i < 5; i++) {
-            executorService.submit(new Ship(Ordered_queue
-));
+        while(i<5) {
+            executorService.submit(new Ship(Ordered_queue));
+            i++;
         }
-        executorService.shutdown();
-    }
+        executorService.shutdown();    }
 }
